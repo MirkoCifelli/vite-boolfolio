e31a6a8cf522d4103a4a92ea60ba80c9f5dd1f8f
MirkoCifelli <ciffo4@hotmail.it>
2024-03-13 17:21:53 +0100

    Chenge technologie not found in page

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -14,12 +14,24 @@ export default {
     <div class="col-4">
         <div class="card" style="width: 18rem">
             <div v-if="project.cover_img != null">
-                <img :src="'http:/127.0.0.1:8000/storage' + project.cover_img" class="card-img-top" :alt="project.title" />
+                <img
+                    :src="'http:/127.0.0.1:8000/storage' + project.cover_img"
+                    class="card-img-top"
+                    :alt="project.title"
+                />
             </div>
             <div class="card-body">
+                <h5>{{ project.id }}</h5>
                 <h5 class="card-title">{{ project.title }}</h5>
                 <h6>{{ project.type.title }}</h6>
-                <h6>{{ project.technologies.title }}</h6>
+                <!-- <h6>{{ project.technologies.title }}</h6> -->
+                <span
+                    v-for="technologie in project.technologies"
+                    :kei="technologie.id"
+                    class="ms-3"
+                >
+                    {{ technologie.title }}
+                </span>
                 <p class="card-text">{{ project.content }}.</p>
             </div>
         </div>
